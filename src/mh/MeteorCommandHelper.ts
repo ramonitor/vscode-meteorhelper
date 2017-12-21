e05eeeb243b9fc2dbf3d73cd14b65f85fd4f6e1e
Lauren ten Hoor <laurentenhoor@gmail.com>
2017-12-21 04:28:54 +0100

    Also removed the trainling dashed line

--- src/mh/MeteorCommandHelper.ts
+++ src/mh/MeteorCommandHelper.ts
@@ -86,9 +86,7 @@ class MeteorCommand {
             let output = '';
 
             const envArgsString = this.environmentArgs ? ConfigHelper.getEnvironmentArgsConfigString(this.environmentArgs) : '';
-            this.outputChannel.append(this, '--------------------------------------------------------------------------------------------------\n');
             this.outputChannel.append(this, `MeteorHelper: executing '${envArgsString}${command}'..\n`);
-            this.outputChannel.append(this, '--------------------------------------------------------------------------------------------------\n');
             
             const processEnv = Object.assign({}, process.env, this.environmentArgs);
             this.process = spawn(meteorPath, this.arguments, { cwd, env: processEnv });
@@ -129,7 +127,7 @@ class MeteorCommand {
                     }
                     reject(output);
                 }
-                this.outputChannel.append(this, '\n--------------------------------------------------------------------------------------------------\n');
+                this.outputChannel.append(this, '\n');
             });
         });
     }
